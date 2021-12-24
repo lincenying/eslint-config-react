733ba1206590f3090768cca5134db21fe9d78e7b
林岑影 <lincenying@qq.com>
2021-12-24 16:49:12 +0800

    commit on 2021-12-24 16:49:12

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = {
     node: true
   },
   extends: ["eslint:recommended", "prettier"],
-  parser: "babel-eslint",
+  parser: "@babel/eslint-parser",
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: "module",
